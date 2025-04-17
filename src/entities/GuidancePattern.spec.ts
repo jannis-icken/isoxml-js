4abf01c1354764c74f53d030306494d900872655
Jannis Icken <jannis.icken@amazone.de>
2025-04-17 08:28:16 +0200

    add structure for GuidancePattern entity tests

---
+++ src/entities/GuidancePattern.spec.ts
@@ -0,0 +1,38 @@
+
+describe('GuidancePattern Entity', () => {
+
+    it('should create GuidancePattern GeoJSON of type A+', async () => {
+        // GPN3
+        return
+    })
+
+    it('should create GuidancePattern GeoJSON of type AB', async () => {
+        // GPN1 or GPN2
+        return
+    })
+
+    it('should extend GuidancePatterns in GeoJSON', async () => {
+        // GPN2
+        return
+    })
+
+    it('should propagate GuidancePatterns in GeoJSON', async () => {
+        // GPN1
+        return
+    })
+
+    it('should consider the swathWidth when propagating in GeoJSON', async () => {
+        // GPN1
+        return
+    })
+
+    it('should apply BoundaryPolygons in GeoJSON', async () => {
+        // GPN1! (GPN2 only has BoundaryPolygon of GuidanceGROUP -> extra test file)
+        return
+    })
+
+    it('should apply GuidanceShift in GeoJSON', async () => {
+        return
+    })
+
+})
